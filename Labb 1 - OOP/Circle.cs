b96ffa715aea7510a6671f28e5d4df161d5dc198
filip <filip@DESKTOP-EQEV834>
2024-10-27 12:46:43 +0100

    Add project files.

---
+++ Labb 1 - OOP/Circle.cs
@@ -0,0 +1,23 @@
+﻿namespace Labb_1___OOP
+{ 
+        public class Circle
+        {
+            public float _pi = 3.141f;
+            public int _Radius;
+
+            public Circle(int radie)
+            {
+                this._Radius = radie;
+
+            }
+
+            public float getArea()
+            {
+                float result = _Radius * _Radius * _pi;
+                return result;
+
+            }
+        }
+
+ }
+
